a93a5edf0bd8f730b3d138af601296a2c3fb513f
Michiel Meersmans <maigel5000@gmail.com>
2012-02-27 00:28:01 +0100

    Changed some shit
    Signed-off-by: Michiel Meersmans <maigel5000@gmail.com>

---
+++ SeriesTracker/ManagePanel.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace SeriesTracker
+{
+    partial class ManagePanel
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.addSerieButton = new System.Windows.Forms.Button();
+            this.removeSerieButton = new System.Windows.Forms.Button();
+            this.currentSeries = new System.Windows.Forms.ListBox();
+            this.newSeries = new System.Windows.Forms.ListBox();
+            this.seriesSearchButton = new System.Windows.Forms.Button();
+            this.seriesSearchBox = new System.Windows.Forms.TextBox();
+            this.searchSeriesWorker = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // addSerieButton
+            // 
+            this.addSerieButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addSerieButton.Location = new System.Drawing.Point(371, 94);
+            this.addSerieButton.Name = "addSerieButton";
+            this.addSerieButton.Size = new System.Drawing.Size(25, 25);
+            this.addSerieButton.TabIndex = 11;
+            this.addSerieButton.Text = "<";
+            this.addSerieButton.UseVisualStyleBackColor = true;
+            this.addSerieButton.Click += new System.EventHandler(this.addSerie_Click);
+            // 
+            // removeSerieButton
+            // 
+            this.removeSerieButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.removeSerieButton.Location = new System.Drawing.Point(371, 125);
+            this.removeSerieButton.Name = "removeSerieButton";
+            this.removeSerieButton.Size = new System.Drawing.Size(25, 25);
+            this.removeSerieButton.TabIndex = 10;
+            this.removeSerieButton.Text = ">";
+            this.removeSerieButton.UseVisualStyleBackColor = true;
+            this.removeSerieButton.Click += new System.EventHandler(this.removeSerieButton_Click);
+            // 
+            // currentSeries
+            // 
+            this.currentSeries.FormattingEnabled = true;
+            this.currentSeries.Location = new System.Drawing.Point(12, 6);
+            this.currentSeries.Name = "currentSeries";
+            this.currentSeries.Size = new System.Drawing.Size(356, 238);
+            this.currentSeries.TabIndex = 9;
+            // 
+            // newSeries
+            // 
+            this.newSeries.FormattingEnabled = true;
+            this.newSeries.Location = new System.Drawing.Point(400, 6);
+            this.newSeries.Name = "newSeries";
+            this.newSeries.Size = new System.Drawing.Size(356, 238);
+            this.newSeries.TabIndex = 8;
+            // 
+            // seriesSearchButton
+            // 
+            this.seriesSearchButton.Location = new System.Drawing.Point(681, 250);
+            this.seriesSearchButton.Name = "seriesSearchButton";
+            this.seriesSearchButton.Size = new System.Drawing.Size(75, 23);
+            this.seriesSearchButton.TabIndex = 7;
+            this.seriesSearchButton.Text = "Search";
+            this.seriesSearchButton.UseVisualStyleBackColor = true;
+            this.seriesSearchButton.Click += new System.EventHandler(this.seriesSearchButton_Click);
+            // 
+            // seriesSearchBox
+            // 
+            this.seriesSearchBox.Location = new System.Drawing.Point(511, 253);
+            this.seriesSearchBox.Name = "seriesSearchBox";
+            this.seriesSearchBox.Size = new System.Drawing.Size(164, 20);
+            this.seriesSearchBox.TabIndex = 6;
+            // 
+            // searchSeriesWorker
+            // 
+            this.searchSeriesWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.searchSeriesWorker_DoWork);
+            this.searchSeriesWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.searchSeriesWorker_RunWorkerCompleted);
+            // 
+            // ManagePanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.addSerieButton);
+            this.Controls.Add(this.removeSerieButton);
+            this.Controls.Add(this.currentSeries);
+            this.Controls.Add(this.newSeries);
+            this.Controls.Add(this.seriesSearchButton);
+            this.Controls.Add(this.seriesSearchBox);
+            this.Name = "ManagePanel";
+            this.Size = new System.Drawing.Size(774, 316);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button addSerieButton;
+        private System.Windows.Forms.Button removeSerieButton;
+        private System.Windows.Forms.ListBox currentSeries;
+        private System.Windows.Forms.ListBox newSeries;
+        private System.Windows.Forms.Button seriesSearchButton;
+        private System.Windows.Forms.TextBox seriesSearchBox;
+        private System.ComponentModel.BackgroundWorker searchSeriesWorker;
+    }
+}
---
+++ SeriesTracker/ManagePanel.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SeriesTracker
+{
+    public partial class ManagePanel : UserControl
+    {
+        public ManagePanel()
+        {
+            InitializeComponent();
+            loadSeries();
+        }
+
+        protected override void OnSizeChanged(EventArgs e)
+        {
+            base.OnSizeChanged(e);
+            resizeObjects();
+        }
+
+        private void resizeObjects()
+        {
+            newSeries.Width = this.Width / 2 - 30;
+            newSeries.Location = new System.Drawing.Point(this.Width - newSeries.Width - 12, currentSeries.Location.Y);
+            currentSeries.Width = this.Width / 2 - 30;
+            int middle = currentSeries.Location.Y + currentSeries.Height / 2 - removeSerieButton.Height / 2;
+            removeSerieButton.Location = new System.Drawing.Point(currentSeries.Location.X + 6 + currentSeries.Width, middle + 15);
+            addSerieButton.Location = new System.Drawing.Point(currentSeries.Location.X + 6 + currentSeries.Width, middle - 15);
+            seriesSearchButton.Location = new System.Drawing.Point(
+                newSeries.Location.X + newSeries.Width - seriesSearchButton.Width, newSeries.Location.Y + newSeries.Height + 6);
+            seriesSearchBox.Location = new System.Drawing.Point(seriesSearchButton.Location.X - seriesSearchBox.Width - 6, seriesSearchButton.Location.Y);
+        }
+
+        private void loadSeries()
+        {
+            SQLiteDatabase db = new SQLiteDatabase();
+            currentSeries.Items.AddRange(db.getAllSeries().ToArray());
+        }
+
+        private void addSerie(Serie serie)
+        {
+            if (serie == null)
+                return;
+            if (!currentSeries.Items.Contains(serie))
+            {
+                currentSeries.Items.Add(serie);
+                newSeries.Items.Remove(serie);
+                serie.Save();
+            }
+        }
+
+        private void deleteSerie(Serie serie)
+        {
+            if (MessageBox.Show("Are you sure you want to delete the series?", "Confirm delete", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                SQLiteDatabase db = new SQLiteDatabase();
+                
+            }
+        }
+
+        private void seriesSearchButton_Click(object sender, EventArgs e)
+        {
+            seriesSearchButton.Enabled = false;
+            seriesSearchButton.Text = "Searching...";
+            searchSeriesWorker.RunWorkerAsync();
+        }
+
+        private void searchSeriesWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            ApiReader reader = new ApiReader();
+            e.Result = reader.GetSeriesFromName(seriesSearchBox.Text);
+        }
+
+        private void searchSeriesWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            newSeries.Items.Clear();
+            foreach (Serie serie in (List<Serie>)e.Result)
+                if (!currentSeries.Items.Contains(serie))
+                    newSeries.Items.Add(serie);
+            seriesSearchButton.Enabled = true;
+            seriesSearchButton.Text = "Search";
+        }
+
+        private void addSerie_Click(object sender, EventArgs e)
+        {
+            addSerie((Serie)newSeries.SelectedItem);
+        }
+
+        private void removeSerieButton_Click(object sender, EventArgs e)
+        {
+            deleteSerie((Serie)currentSeries.SelectedItem);
+        }
+    }   
+}
